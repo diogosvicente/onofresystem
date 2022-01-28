25b5ec42cff2eb66ec6e2e90408b013199627f44
Diogo <diogosk84ever@hotmail.com>
2022-01-28 17:33:27 -0300

    primeira versao WhatsApp que Thiaguinho enviou

---
+++ thiaguinho/th01.cpp
@@ -0,0 +1,19 @@
+Float nota1, nota2, nota3, nota4, nota5, mídia;
+Main()
+{
+
+Printf(" primeira nota:");
+Scanf("%f", &nota1);
+Printf ("segunda nota:");
+Scanf("%f", &nota2);
+Printf("terceira nota:");
+Scanf("%f", &nota3:);
+Printf("quarta nota:");
+Scanf("%f", &nota4:");
+Printf("quinta nota:");
+Scanf("%f", &nota5:");
+Media= (nota1+nota2+nota3+nota4+nota5+) / 5;
+Printf("a nota dos aluno sera %f.", media);
+getch ();
+
+}
